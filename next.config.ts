07a88fe1a09083777d21970f6ff2a64a3a1e90ba
RicardoDS7 <ricardo.desousa96@gmail.com>
2025-05-21 12:35:04 +0200

    Deploy nested out folder

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export', // this makes it a static site
-  basePath: '/phoenix-dashboard', // match the repo name if it's a project page
   trailingSlash: true, // optional: improves compatibility with GitHub Pages
 };
 
