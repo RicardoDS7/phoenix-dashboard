60b6af908140d4b7537701dfa7694ee16c16cf16
RicardoDS7 <ricardo.desousa96@gmail.com>
2025-05-21 13:49:17 +0200

    Fix links

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,8 @@
 import type { NextConfig } from "next";
 
-const isDev = process.env.NODE_ENV === "development";
-
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
-
-  // always expose this so you can reference it in code
-  env: {
-    NEXT_PUBLIC_BASE_PATH: isDev ? "" : "/phoenix-dashboard",
-  },
-
-  // only apply these in production
-  ...(isDev
-    ? {}
-    : {
-        basePath: "/phoenix-dashboard",    // ↩️ no trailing slash
-        assetPrefix: "/phoenix-dashboard", // ↩️ no trailing slash
-      }),
 };
 
 export default nextConfig;
