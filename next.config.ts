515fd961ef9cfa10e24625c6ba407abe40924654
RicardoDS7 <ricardo.desousa96@gmail.com>
2025-05-21 11:40:28 +0200

    Add worfklow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // this makes it a static site
+  basePath: '/phoenix-dashboard', // match the repo name if it's a project page
+  trailingSlash: true, // optional: improves compatibility with GitHub Pages
 };
 
 export default nextConfig;
